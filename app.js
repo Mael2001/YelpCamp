b4c0e5bec943a99d82541e31f0e9e6907cdc5fdf
Mario Flores <mariof2001@unitec.edu>
2023-02-09 11:12:59 -0600

    JOI Validation Middleware

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
+const {campgroundSchema} = require('./schemas.js')
 const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
@@ -37,25 +37,20 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new')
 })
 
-app.post('/campgrounds', catchAsync(async (req, res) => {
-    //if (!req.body.Campground) throw new ExpressError('Invalid Campground Data', 400);
-    const campgroundSchema = Joi.object({
-        campground: Joi.object({
-            title: Joi.string().required(),
-            price: Joi.number().required().min(0),
-            image: Joi.string().required(),
-            location: Joi.string().required(),
-            description: Joi.string().required()
-        }).required()
-    })
+const validateCampground = (req, res, next) =>{
     const { error } = campgroundSchema.validate(req.body);
     if (error) {
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
     }
+    next();
+}
+
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
+    //if (!req.body.Campground) throw new ExpressError('Invalid Campground Data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save()
-    res.redirect(`/campground/${campground._id}`)
+    res.redirect(`/campgrounds/${campground._id}`)
 }))
 
 app.get('/campgrounds/:id', catchAsync(async (req, res) => {
@@ -97,9 +92,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.all('*', (req, res, next) => {
-    next(new ExpressError('Page Not Found', 404))
-})
 
 app.listen(3000, () => {
     console.log('serving port 3000')
