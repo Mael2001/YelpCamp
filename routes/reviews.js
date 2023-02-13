c03ca82d8283eec69ea91a411818127494d5d458
Mario Flores <mariof2001@unitec.edu>
2023-02-13 15:12:11 -0600

    Reviews Permissions

--- routes/reviews.js
+++ routes/reviews.js
@@ -9,10 +9,12 @@ router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     const review = new Review(req.body.review);
-    console.log('review new')
+
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+
     req.flash('success', 'Successfully created review');
     res.redirect(`/campgrounds/${campground._id}`)
 }))
@@ -20,7 +22,8 @@ router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
 router.delete('/:reviewId', isLoggedIn, catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
-    const review = await Review.findByIdAndDelete(reviewId);
+    const review = await Review.findOneAndDelete({ _id: reviewId });
+
     req.flash('success', 'Successfully deleted review');
     res.redirect(`/campgrounds/${campground._id}`)
 }))
