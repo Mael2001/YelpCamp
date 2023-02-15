f1434cdf62ffb8f8bd1e8ce9a56e5c4d28bf2ef9
Mario Flores <mariof2001@unitec.edu>
2023-02-15 14:51:22 -0600

    Centering Map on Campground

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -1,7 +1,13 @@
 mapboxgl.accessToken = mapToken;
+var campgroundInfo = JSON.parse(campground);
+console.log(campgroundInfo.geometry)
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 4, // starting zoom
+    center: [ ...campgroundInfo.geometry.coordinates ], // starting position [lng, lat]
+    zoom: 8, // starting zoom
 });
+
+var marker = new mapboxgl.Marker()
+    .setLngLat([ ...campgroundInfo.geometry.coordinates ])
+    .addTo(map);
