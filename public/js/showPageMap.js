d1a4dc3ac434a987dec31492f807db6e1853596d
Mario Flores <mariof2001@unitec.edu>
2023-02-15 15:10:50 -0600

    Customizing Map Popup

--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -3,11 +3,17 @@ var campgroundInfo = JSON.parse(campground);
 console.log(campgroundInfo.geometry)
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    style: 'mapbox://styles/mapbox/light-v11', // style URL
     center: [ ...campgroundInfo.geometry.coordinates ], // starting position [lng, lat]
     zoom: 8, // starting zoom
 });
 
 var marker = new mapboxgl.Marker()
     .setLngLat([ ...campgroundInfo.geometry.coordinates ])
+    .setPopup(
+        new mapboxgl.Popup({offset: 25})
+        .setHTML(
+            `<h4>${campgroundInfo.title}</h4> <p>${campgroundInfo.location}</p>`
+        )
+    )
     .addTo(map);
