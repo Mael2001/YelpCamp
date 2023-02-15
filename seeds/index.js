b0eee7a89cd62996a7ac538d646912a9e6a549b5
Mario Flores <mariof2001@unitec.edu>
2023-02-15 13:12:50 -0600

    Fixing Seed Files

--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,36 @@ const seedDB = async () => {
             author: '63ea9252f32c5a51c90b8e56',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
+            images: [
+                {
+                    url: 'https://source.unsplash.com/collection/483251',
+                    filename:'Unsplash'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dzyv9g3yh/image/upload/v1676487915/YelpCamp/daoxldarexkrlrqbqd2x.jpg',
+                    filename: 'YelpCamp/daoxldarexkrlrqbqd2x'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dzyv9g3yh/image/upload/v1676487915/YelpCamp/o22t52icmsnc0sndnhj1.jpg',
+                    filename: 'YelpCamp/o22t52icmsnc0sndnhj1'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dzyv9g3yh/image/upload/v1676487915/YelpCamp/tknxtsw0bf8azybgliyn.png',
+                    filename: 'YelpCamp/tknxtsw0bf8azybgliyn'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dzyv9g3yh/image/upload/v1676487916/YelpCamp/ajwpklwayjd38kiaojiv.png',
+                    filename: 'YelpCamp/ajwpklwayjd38kiaojiv'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dzyv9g3yh/image/upload/v1676487916/YelpCamp/iswxqadkcytakflmeuib.png',
+                    filename: 'YelpCamp/iswxqadkcytakflmeuib'
+                },
+                {
+                    url: 'https://res.cloudinary.com/dzyv9g3yh/image/upload/v1676487916/YelpCamp/rxg1ientw3pqv9zcgavs.png',
+                    filename: 'YelpCamp/rxg1ientw3pqv9zcgavs'
+                }
+            ],
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat dolorem error officia. Doloremque, rem ab sunt id vel laudantium, praesentium nam laborum suscipit, iusto exercitationem ipsum placeat similique et aliquid.',
             price: price
         }).save()
