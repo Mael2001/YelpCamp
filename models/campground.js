cab30a3bd53ded0d5b5d9ab26b3258a6b1b8ff7a
Mario Flores <mariof2001@unitec.edu>
2023-02-15 14:05:53 -0600

    Making thumbnail Virtual Property

--- models/campground.js
+++ models/campground.js
@@ -2,15 +2,23 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review')
 
+//https://res.cloudinary.com/demo/image/upload/c_crop,g_face,h_400,w_400/r_max/c_scale,w_200/lady.jpg
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200')
+})
+
 const campgroundSchema = new Schema({
     title: String,
     price: Number,
     location: String,
     images: [
-        {
-            url: String,
-            filename: String
-        }
+        ImageSchema
     ],
     description: String,
     author: {
@@ -24,11 +32,11 @@ const campgroundSchema = new Schema({
         }
     ]
 })
-campgroundSchema.post('findOneAndDelete', async function (doc){
-    if(doc){
+campgroundSchema.post('findOneAndDelete', async function (doc) {
+    if (doc) {
         await Review.deleteMany({
-            _id:{
-                $in:doc.reviews
+            _id: {
+                $in: doc.reviews
             }
         })
     }
