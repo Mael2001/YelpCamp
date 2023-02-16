626aa9202502638374f444843fef88f16363f5a5
Mario Flores <mariof2001@unitec.edu>
2023-02-15 20:44:50 -0600

    Styling App

--- public/js/indexClusterMap.js
+++ public/js/indexClusterMap.js
@@ -1,6 +1,6 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/dark-v11',
     center: [-103.5917, 40.6699],
@@ -129,4 +129,5 @@ map.on('load', () => {
     map.on('mouseleave', 'clusters', () => {
         map.getCanvas().style.cursor = '';
     });
+    map.addControl(new mapboxgl.NavigationControl());
 });
--- public/js/showPageMap.js
+++ public/js/showPageMap.js
@@ -3,16 +3,18 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/light-v11', // style URL
-    center: [ ...campgroundInfo.coordinates], // starting position [lng, lat]
+    center: [...campgroundInfo.coordinates], // starting position [lng, lat]
     zoom: 8, // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
 var marker = new mapboxgl.Marker()
-    .setLngLat([ ...campgroundInfo.coordinates ])
+    .setLngLat([...campgroundInfo.coordinates])
     .setPopup(
-        new mapboxgl.Popup({offset: 25})
-        .setHTML(
-            `<h4>${campgroundInfo.title}</h4> <p>${campgroundInfo.location}</p>`
-        )
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h4>${campgroundInfo.title}</h4> <p>${campgroundInfo.location}</p>`
+            )
     )
     .addTo(map);
