0ed8d67eb77df11bbe8fb8d79f110ef62aafb126
Mario Flores <mariof2001@unitec.edu>
2023-02-16 11:02:37 -0600

    Minor fixes for Heroku

--- app.js
+++ app.js
@@ -146,6 +146,8 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 });
 
-app.listen(3000, () => {
-    console.log('serving port 3000')
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`serving port ${port}`)
 });
