85adef6db8c837bd5de982b3986f10a4fcd4a3d1
Mario Flores <mariof2001@unitec.edu>
2023-02-16 10:08:01 -0600

    Helmet Introduction

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-if(process.env.NODE_ENV !== "production"){
+if (process.env.NODE_ENV !== "production") {
     require('dotenv').config()
-}
+};
+
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -8,15 +9,16 @@ const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const session = require('express-session');
-const flash = require('connect-flash')
+const flash = require('connect-flash');
 const passport = require('passport');
 const localStrategy = require('passport-local');
-const User = require('./models/user')
-const mongoSanitize = require('express-mongo-sanitize')
+const User = require('./models/user');
+const mongoSanitize = require('express-mongo-sanitize');
+const helmet = require('helmet');
 
-const userRoutes = require('./routes/users')
-const campgroundsRoutes = require('./routes/campgrounds')
-const reviewsRoutes = require('./routes/reviews')
+const userRoutes = require('./routes/users');
+const campgroundsRoutes = require('./routes/campgrounds');
+const reviewsRoutes = require('./routes/reviews');
 
 mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
@@ -26,17 +28,17 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
     console.log("DB Connected")
 })
-app.engine('ejs', ejsMate)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-app.use(express.static(path.join(__dirname, 'public')))
-app.use(mongoSanitize())
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
-    name:'yelpCampSession',
+    name: 'yelpCampSession',
     secret: 'secret',
     resave: false,
     saveUninitialized: true,
@@ -46,9 +48,14 @@ const sessionConfig = {
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
-}
-app.use(session(sessionConfig))
-app.use(flash())
+};
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(
+    helmet({
+        contentSecurityPolicy: false
+    })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -58,7 +65,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
-    if(!['/login', '/'].includes(req.originalUrl)){
+    if (!['/login', '/'].includes(req.originalUrl)) {
         req.session.returnTo = req.originalUrl;
     }
     res.locals.currentUser = req.user;
@@ -66,13 +73,13 @@ app.use((req, res, next) => {
     res.locals.error = req.flash('error');
     res.locals.info = req.flash('info');
     next();
-})
+});
 
 
 app.get('/', (req, res) => {
     res.render('home')
-})
-app.use('/',userRoutes)
+});
+app.use('/', userRoutes);
 app.use('/campgrounds', campgroundsRoutes);
 app.use('/campgrounds/:id/reviews', reviewsRoutes);
 
@@ -80,8 +87,8 @@ app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
     if (!err.message) err.message = 'Oh no, Something went wrong'
     res.status(statusCode).render('error', { err })
-})
+});
 
 app.listen(3000, () => {
     console.log('serving port 3000')
-})
+});
